8a2db7c70ae7cc32bb3670a29fcabf40baa86288
dotienanh77 <anhdt.ute@gmail.com>
2021-08-24 10:38:27 +0700

    renderHeader and logo in signup screen

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,11 +1,76 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {Text, View} from 'react-native';
-
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Image,
+  TextInput,
+  Modal,
+  Platform,
+  Flatlist,
+  KetboarAvoidingView,
+  ScrollView,
+  KeyboardAvoidingView,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import {COLORS, FONTS, SIZES, icons, images} from '../constants';
 const SignUp = () => {
+  function renderHeader() {
+    return (
+      <TouchableOpacity
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          marginTop: SIZES.padding * 6,
+          paddingHorizontal: SIZES.padding * 2,
+        }}
+        onPress={() => console.log('Sign Up')}>
+        <Image
+          source={icons.back}
+          resizeMode="contain"
+          style={{width: 20, height: 20, tintColor: COLORS.white}}
+        />
+        <Text
+          style={{
+            marginLeft: SIZES.padding * 1.5,
+            color: COLORS.white,
+            ...FONTS.h4,
+          }}>
+          Sign Up
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+  function renderLogo() {
+    return (
+      <View
+        style={{
+          marginTop: SIZES.padding * 5,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Image
+          source={images.wallieLogo}
+          resizeMode="contain"
+          style={{width: '60%'}}
+        />
+      </View>
+    );
+  }
   return (
-    <View>
-      <Text>SignupScreen</Text>
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : null}
+      style={{flex: 1}}>
+      <LinearGradient colors={[COLORS.lime, COLORS.emerald]} style={{flex: 1}}>
+        <ScrollView>
+          {renderHeader()}
+          {renderLogo()}
+        </ScrollView>
+      </LinearGradient>
+    </KeyboardAvoidingView>
   );
 };
 export default SignUp;
