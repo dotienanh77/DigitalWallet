283a69bc91d93464cc5ac8c4812982261129ea16
dotienanh77 <anhdt.ute@gmail.com>
2021-08-24 16:43:43 +0700

    renderForm UI

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -60,6 +60,121 @@ const SignUp = () => {
       </View>
     );
   }
+  function renderForm() {
+    return (
+      <View
+        style={{
+          marginTop: SIZES.padding * 3,
+          marginHorizontal: SIZES.padding * 3,
+        }}>
+        {/* // Full name */}
+        <View style={{marginTop: SIZES.padding * 3}}>
+          <Text style={{color: COLORS.lightGreen, ...FONTS.body3}}>
+            Full Name
+          </Text>
+          <TextInput
+            style={{
+              marginVertical: SIZES.padding,
+              borderBottomColor: COLORS.white,
+              borderBottomWidth: 1,
+              height: 40,
+              color: COLORS.white,
+              ...FONTS.body3,
+            }}
+            placeholder="Enter Full Name"
+            placeholderTextColor={COLORS.white}
+            selectionColor={COLORS.white}
+          />
+        </View>
+        {/* Phone Number */}
+        <View style={{marginTop: SIZES.padding * 2}}>
+          <Text style={{color: COLORS.lightGreen, ...FONTS.body3}}>
+            Phone Number
+          </Text>
+          <View style={{flexDirection: 'row'}}>
+            <TouchableOpacity
+              style={{
+                width: 100,
+                height: 50,
+                marginHorizontal: 5,
+                borderBottomColor: COLORS.white,
+                borderBottomWidth: 1,
+                flexDirection: 'row',
+                ...FONTS.body2,
+              }}
+              onPress={() => console.log('show modal')}>
+              <View style={{justifyContent: 'center'}}>
+                <Image
+                  source={icons.down}
+                  style={{height: 10, width: 10, tintColor: COLORS.white}}
+                />
+              </View>
+              <View style={{justifyContent: 'center', marginLeft: 5}}>
+                <Image
+                  source={images.usFlag}
+                  resizeMode="contain"
+                  style={{width: 30, height: 30}}
+                />
+              </View>
+              <View style={{justifyContent: 'center', marginLeft: 5}}>
+                <Text style={{color: COLORS.white, ...FONTS.body3}}>US +1</Text>
+              </View>
+            </TouchableOpacity>
+            {/* Phone Number */}
+            <TextInput
+              style={{
+                flex: 1,
+                marginVertical: SIZES.padding,
+                borderBottomColor: COLORS.white,
+                borderBottomWidth: 1,
+                height: 40,
+                color: COLORS.white,
+                ...FONTS.body3,
+              }}
+              placeholder="Enter Phone Number"
+              placeholderTextColor={COLORS.white}
+              selectionColor={COLORS.white}
+            />
+          </View>
+        </View>
+        {/* Password */}
+        <View style={{marginTop: SIZES.padding * 2}}>
+          <Text style={{color: COLORS.lightGreen, ...FONTS.body3}}>
+            Password
+          </Text>
+          <TextInput
+            style={{
+              marginVertical: SIZES.padding,
+              borderBottomColor: COLORS.white,
+              borderBottomWidth: 1,
+              height: 40,
+              color: COLORS.white,
+              ...FONTS.body3,
+            }}
+            placeholder="Enter password"
+            placeholderTextColor={COLORS.white}
+            selectionColor={COLORS.white}
+            secureTextEntry={true}
+          />
+          <TouchableOpacity
+            style={{
+              position: 'absolute',
+              right: 0,
+              bottom: 10,
+              height: 30,
+              width: 30,
+            }}
+            onPress={() => console.log('Toggle')}>
+            <Image
+              source={icons.eye}
+              style={{height: 20, width: 20, tintColor: COLORS.white}}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : null}
@@ -68,6 +183,7 @@ const SignUp = () => {
         <ScrollView>
           {renderHeader()}
           {renderLogo()}
+          {renderForm()}
         </ScrollView>
       </LinearGradient>
     </KeyboardAvoidingView>
