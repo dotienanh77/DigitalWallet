23d636d299a2c74caebf62eefb43f290866f2265
dotienanh77 <anhdt.ute@gmail.com>
2021-08-25 21:47:15 +0700

    render header - scan screen

--- screens/Scan.js
+++ screens/Scan.js
@@ -1,8 +1,53 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {Text, View, Image, TouchableOpacity} from 'react-native';
 import {RNCamera} from 'react-native-camera';
 import {FONTS, SIZES, COLORS, icons, images} from '../constants';
-const Scan = () => {
+
+const Scan = ({navigation}) => {
+  function renderHeader() {
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          marginTop: SIZES.padding * 4,
+          paddingHorizontal: SIZES.padding * 3,
+        }}>
+        <TouchableOpacity
+          style={{
+            width: 45,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+          onPress={() => navigation.navigate('Home')}>
+          <Image
+            source={icons.close}
+            style={{height: 20, width: 20, tintColor: COLORS.white}}
+          />
+        </TouchableOpacity>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Text style={{...FONTS.body3, color: COLORS.white}}>
+            Scan for Payment
+          </Text>
+        </View>
+        <TouchableOpacity
+          style={{
+            width: 45,
+            height: 45,
+            borderRadius: 10,
+            backgroundColor: COLORS.green,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+          onPress={() => console.log('Infor')}>
+          <Image
+            source={icons.info}
+            style={{height: 25, width: 25, tintColor: COLORS.white}}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
   return (
     <View style={{flex: 1, backgroundColor: COLORS.transparent}}>
       <RNCamera
@@ -18,8 +63,9 @@ const Scan = () => {
           message: 'Camera is required for Barcode scanning',
           buttonPositive: 'Ok',
           buttonNegative: 'Cancel',
-        }}
-      />
+        }}>
+        {renderHeader()}
+      </RNCamera>
     </View>
   );
 };
