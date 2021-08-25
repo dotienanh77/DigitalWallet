494025661b2012270db5093399fd12198ae7877a
dotienanh77 <anhdt.ute@gmail.com>
2021-08-25 11:37:20 +0700

    custom button tab bar navigation

--- navigation/tab.js
+++ navigation/tab.js
@@ -1,6 +1,6 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View, Text, Image, TouchableOpacity} from 'react-native';
+import {View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
 import {
   createBottomTabNavigator,
   BottomTabBar,
@@ -12,12 +12,69 @@ import {COLORS, icons} from '../constants';
 
 const Tab = createBottomTabNavigator();
 
+const TabBarCustomButton = ({
+  accessibilityLabel,
+  accessibilityState,
+  children,
+  onPress,
+}) => {
+  let isSelected = accessibilityState.selected;
+
+  if (isSelected) {
+    return (
+      <View style={{flex: 1, alignItems: 'center'}}>
+        <View style={{flexDirection: 'row', position: 'absolute', top: 0}}>
+          <View style={{flex: 1, backgroundColor: COLORS.white}} />
+          <Svg width={75} height={61} viewBox="0 0 75 61">
+            <Path
+              d="M75.2 0v61H0V0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"
+              fill={COLORS.white}
+            />
+          </Svg>
+          <View style={{flex: 1, backgroundColor: COLORS.white}} />
+        </View>
+
+        <TouchableOpacity
+          style={{
+            top: -22.5,
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: 50,
+            height: 50,
+            borderRadius: 25,
+            backgroundColor: COLORS.primary,
+            ...styles.shadow,
+          }}
+          onPress={onPress}>
+          {children}
+        </TouchableOpacity>
+      </View>
+    );
+  } else {
+    return (
+      <TouchableOpacity
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: 50,
+          height: 50,
+          backgroundColor: COLORS.white,
+        }}
+        activeOpacity={1}
+        onPress={onPress}>
+        {children}
+      </TouchableOpacity>
+    );
+  }
+};
+
 const Tabs = () => {
   return (
     <Tab.Navigator
-      //   screenOptions={{
-      //     headerShown: false,
-      //   }}
+      screenOptions={{
+        headerShown: false,
+      }}
       tabBarOptions={{
         showLabel: false,
         style: {
@@ -44,6 +101,7 @@ const Tabs = () => {
               }}
             />
           ),
+          tabBarButton: props => <TabBarCustomButton {...props} />,
         }}
       />
       <Tab.Screen
@@ -61,6 +119,7 @@ const Tabs = () => {
               }}
             />
           ),
+          tabBarButton: props => <TabBarCustomButton {...props} />,
         }}
       />
       <Tab.Screen
@@ -78,9 +137,23 @@ const Tabs = () => {
               }}
             />
           ),
+          tabBarButton: props => <TabBarCustomButton {...props} />,
         }}
       />
     </Tab.Navigator>
   );
 };
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: COLORS.primary,
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+});
 export default Tabs;
