cb0ff254c40fed8bd5aca048b6aee4b8e98d761b
dotienanh77 <anhdt.ute@gmail.com>
2021-08-25 22:08:47 +0700

    renderPaymentMethod

--- screens/Scan.js
+++ screens/Scan.js
@@ -48,6 +48,79 @@ const Scan = ({navigation}) => {
       </View>
     );
   }
+
+  function renderPaymentMethod() {
+    return (
+      <View
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          right: 0,
+          height: 180,
+          padding: SIZES.padding * 3,
+          borderTopLeftRadius: SIZES.radius,
+          borderTopRightRadius: SIZES.radius,
+          backgroundColor: COLORS.white,
+        }}>
+        <Text style={{...FONTS.h4}}>Another payment methods</Text>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            alignItems: 'flex-start',
+            marginTop: SIZES.padding * 2,
+          }}>
+          <TouchableOpacity
+            style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}
+            onPress={() => console.log('Phone number')}>
+            <View
+              style={{
+                width: 40,
+                height: 40,
+                backgroundColor: COLORS.lightpurple,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: 10,
+              }}>
+              <Image
+                source={icons.phone}
+                resizeMode="cover"
+                style={{height: 25, width: 25, tintColor: COLORS.purple}}
+              />
+            </View>
+            <Text style={{marginLeft: SIZES.padding, ...FONTS.body4}}>
+              Phone number
+            </Text>
+          </TouchableOpacity>
+          {/* Barcode */}
+          <TouchableOpacity
+            style={{flexDirection: 'row', alignItems: 'center'}}
+            onPress={() => console.log('Bar code')}>
+            <View
+              style={{
+                width: 40,
+                height: 40,
+                backgroundColor: COLORS.lightGreen,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: 10,
+              }}>
+              <Image
+                source={icons.barcode}
+                resizeMode="cover"
+                style={{height: 25, width: 25, tintColor: COLORS.green}}
+              />
+            </View>
+            <Text style={{marginLeft: SIZES.padding, ...FONTS.body4}}>
+              Bar code
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+
   return (
     <View style={{flex: 1, backgroundColor: COLORS.transparent}}>
       <RNCamera
@@ -65,6 +138,7 @@ const Scan = ({navigation}) => {
           buttonNegative: 'Cancel',
         }}>
         {renderHeader()}
+        {renderPaymentMethod()}
       </RNCamera>
     </View>
   );
