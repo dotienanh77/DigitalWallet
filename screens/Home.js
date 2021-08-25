4b09420b541b87b044f2f2d27015ff01b09911ad
dotienanh77 <anhdt.ute@gmail.com>
2021-08-25 14:58:30 +0700

    add flat list Promos

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,151 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState, useEffect} from 'react';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+  FlatList,
+  ScrollView,
+  Image,
+} from 'react-native';
+import {COLORS, SIZES, FONTS, icons, images} from '../constants';
 
 const Home = () => {
+  const featuresData = [
+    {
+      id: 1,
+      icon: icons.reload,
+      color: COLORS.purple,
+      backgroundColor: COLORS.lightpurple,
+      description: 'Top Up',
+    },
+    {
+      id: 2,
+      icon: icons.send,
+      color: COLORS.yellow,
+      backgroundColor: COLORS.lightyellow,
+      description: 'Transfer',
+    },
+    {
+      id: 3,
+      icon: icons.internet,
+      color: COLORS.primary,
+      backgroundColor: COLORS.lightGreen,
+      description: 'Internet',
+    },
+    {
+      id: 4,
+      icon: icons.wallet,
+      color: COLORS.red,
+      backgroundColor: COLORS.lightRed,
+      description: 'Wallet',
+    },
+    {
+      id: 5,
+      icon: icons.bill,
+      color: COLORS.yellow,
+      backgroundColor: COLORS.lightyellow,
+      description: 'Bill',
+    },
+    {
+      id: 6,
+      icon: icons.game,
+      color: COLORS.primary,
+      backgroundColor: COLORS.lightGreen,
+      description: 'Games',
+    },
+    {
+      id: 7,
+      icon: icons.phone,
+      color: COLORS.red,
+      backgroundColor: COLORS.lightRed,
+      description: 'Mobile Prepaid',
+    },
+    {
+      id: 8,
+      icon: icons.more,
+      color: COLORS.purple,
+      backgroundColor: COLORS.lightpurple,
+      description: 'More',
+    },
+  ];
+
+  const specialPromoData = [
+    {
+      id: 1,
+      img: images.promoBanner,
+      title: 'Bonus Cashback1',
+      description: "Don't miss it. Grab it now!",
+    },
+    {
+      id: 2,
+      img: images.promoBanner,
+      title: 'Bonus Cashback2',
+      description: "Don't miss it. Grab it now!",
+    },
+    {
+      id: 3,
+      img: images.promoBanner,
+      title: 'Bonus Cashback3',
+      description: "Don't miss it. Grab it now!",
+    },
+    {
+      id: 4,
+      img: images.promoBanner,
+      title: 'Bonus Cashback4',
+      description: "Don't miss it. Grab it now!",
+    },
+  ];
+
+  const [features, setFeatures] = useState(featuresData);
+  const [specialPromos, setSpecialPromos] = useState(specialPromoData);
+
+  function renderPromos() {
+    const renderItem = ({item}) => (
+      <TouchableOpacity
+        style={{
+          marginVertical: SIZES.base,
+          width: SIZES.width / 2.5,
+        }}
+        onPress={() => console.log(item.title)}>
+        <View
+          style={{
+            height: 80,
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            backgroundColor: COLORS.primary,
+          }}>
+          <Image
+            source={images.promoBanner}
+            resizeMode="cover"
+            style={{
+              width: '100%',
+              height: '100%',
+              borderTopLeftRadius: 20,
+              borderTopRightRadius: 20,
+            }}
+          />
+        </View>
+      </TouchableOpacity>
+    );
+
+    return (
+      <FlatList
+        contentContainerStyle={{paddingHorizontal: SIZES.padding * 3}}
+        numColumns={2}
+        columnWrapperStyle={{justifyContent: 'space-between'}}
+        data={specialPromos}
+        keyExtractor={item => `${item.id}`}
+        renderItem={renderItem}
+        showsVerticalScrollIndicator={false}
+      />
+    );
+  }
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
+      {renderPromos()}
+    </SafeAreaView>
   );
 };
 export default Home;
